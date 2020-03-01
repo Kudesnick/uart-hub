633c4a95d51ab0c40aa63a0590e73ed219728572
Kudesnick <kudesnick@inbox.ru>
2020-03-01 20:13:58 +0000

    [mod] create function for array sending. Tested on single byte

--- doc/spi-test.py
+++ doc/spi-test.py
@@ -7,6 +7,8 @@ import spidev
 spi = spidev.SpiDev()
 spi.open(0, 0)
 
+ch = [1 << i for i in range(0, 10)]
+
 def send_frame(ch, data):
     global spi
 
@@ -19,13 +21,22 @@ def send_frame(ch, data):
 
     return ret
 
+def send(ch, data):
+    if isinstance(data, int): data = [data]
+    if not isinstance(data, list): return
+
+    res = []
+    for d in data: res.extend(send_frame(ch, d))
+
+    return res
+
 spi.bits_per_word = 8 # other value not supported for BCM2835
 spi.lsbfirst = False
 spi.max_speed_hz = 16000000 # 63 MHz - MOSI limit, 16 MHz - MISO limit
 spi.mode = 0b00 # CPOL0|CPHA0
 
 for i in range(0, 10):
-    val = send_frame(ch[i], 0x10 + i)
+    val = send(ch[i], 0x10 + i)
     print "Result: ", str(val)
 
 spi.close()
