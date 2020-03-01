f8d0ec294da146facc1a9096551805426188b5ff
Kudesnick <kudesnick@inbox.ru>
2020-03-01 22:42:24 +0300

    [upd] test script and results of test

--- doc/spi-test.py
+++ doc/spi-test.py
@@ -7,16 +7,26 @@ import spidev
 spi = spidev.SpiDev()
 spi.open(0, 0)
 
+def send_frame(ch, data):
+    global spi
+
+    send_data = [data & 0xff, ch & 0xFF, (ch >> 8) & 0xff, (ch >> 16) & 0xff]
+
+    spi.cshigh = True
+    spi.cshigh = False
+    ret = spi.xfer3(send_data)
+    spi.cshigh = True
+
+    return ret
+
 spi.bits_per_word = 8 # other value not supported for BCM2835
 spi.lsbfirst = False
 spi.max_speed_hz = 16000000 # 63 MHz - MOSI limit, 16 MHz - MISO limit
 spi.mode = 0b00 # CPOL0|CPHA0
 
-spi.cshigh = True
-spi.cshigh = False
-val = spi.xfer3([0x31, 0x32])
-spi.cshigh = True
-print "Result: ", str(val)
+for i in range(0, 10):
+    val = send_frame(ch[i], 0x10 + i)
+    print "Result: ", str(val)
 
 spi.close()
 sys.exit(0)
